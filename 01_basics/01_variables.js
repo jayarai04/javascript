b141f4c25c827a576153e964e62a5990951a531e
Jaya Rai <raij78068@gmail.com>
2026-02-14 15:02:15 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 14453  
+let accountEmail = "jaya@google.com"
+var accountPassword ="12345"
+accountCity = "varanasi"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+/*
+prefer no to use var
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+ let name = "jaya"
+ let age = 19
+ let isLoggedIn = true
+ let state 
+
+//  type of datatypes
+// number=> 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// undefined 
+// symbol => unique
+
+console.log(typeof unedefined)
+console.log(typeof null)
+console.log(typeof "jaya")
